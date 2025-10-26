2fcc45a9df9c78b61231b26f22e3558e572aced9
Jdemore <Jdemore4@kent.edu>
2025-10-26 02:05:10 -0400

    ~

--- GPIS/Assets/ScriptableObjects/_BaseClasses/BaseNPCDef.cs
+++ GPIS/Assets/ScriptableObjects/_BaseClasses/BaseNPCDef.cs
@@ -1,18 +1,10 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class BaseNPCDef : MonoBehaviour
+public abstract class BaseNPCDef : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("NPC Info")]
+    public string npcID;
+    public string npcName;
+    public Sprite portrait;
+    public string description;
 }
