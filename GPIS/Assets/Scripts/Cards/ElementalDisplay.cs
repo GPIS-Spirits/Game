11a4eded654262e0954f42c8d1800fb5e9e11eab
Pfrankl2 <pfrankl2@kent.edu>
2025-10-27 17:46:09 -0400

    ~

---
+++ GPIS/Assets/Scripts/Cards/ElementalDisplay.cs
@@ -0,0 +1,13 @@
+// Attaches to Game Objects to allow Elemental SO data to be used.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // For is we decide to have visible Elemental Cards.
+
+public class ElementalDisplay : MonoBehaviour
+{
+    public ElementalDef elemental; // Holds "_ElementalDef" SO data.
+
+    // --- Visual card data variables would go here if we decide to use them! ---
+}
---
+++ GPIS/Assets/Scripts/Cards/EnemyDisplay.cs
@@ -0,0 +1,13 @@
+// Attaches to Game Objects to allow Enemy SO data to be used.
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro; // For is we decide to have visible Elemental Cards.
+
+public class EnemyDisplay : MonoBehaviour
+{
+    public EnemyDef enemy; // Holds "_EnemyDef" SO data.
+
+    // --- Visual card data variables would go here if we decide to use them! ---
+}
