e0ded1a2bb68882facb5388710145bae4c7b68c2
Jdemore <Jdemore4@kent.edu>
2025-11-11 11:46:15 -0500

    ~Assets, some cleanup. Dungeon loop gone/in progress

--- GPIS/Assets/ScriptableObjects/_BaseClasses/BaseTileDef.cs
+++ GPIS/Assets/ScriptableObjects/_BaseClasses/BaseTileDef.cs
@@ -4,20 +4,13 @@ using UnityEngine.Tilemaps;
 /// <summary>
 /// Base Class for all dungeon tiles
 /// </summary>
-public abstract class BaseTileDef: ScriptableObject
+public abstract class BaseTileDef : ScriptableObject
 {
-    [SerializeField] private string id;
-    [SerializeField] private string tileName;
-    [SerializeField] private TileBase tile;
-    public string ID => id;
-    public string TileName => tileName;
-    public TileBase Tile => tile;
-}
+    [Header("Identity")]
+    public string id;
+    public string displayName;
+    [TextArea] public string description;
 
-public class DungeonTile
-{
-    public BaseTileDef def;
-    public Vector3Int pos;
-    public bool visited = false;
-
+    [Header("Art")]
+    public Sprite sprite;
 }
