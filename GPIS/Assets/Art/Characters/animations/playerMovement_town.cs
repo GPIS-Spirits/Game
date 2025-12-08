acfe6c30d1de3483e2ec9db6c15efc16e6501cc8
Conner Brunner <connerbrunner@gmail.com>
2025-12-08 14:10:01 -0500

    Added basic Jazz NPC to town
    I added the Jazz character that will do... something. There is a simple funny interaction with him and that's about it.

--- GPIS/Assets/Art/Characters/animations/playerMovement_town.cs
+++ GPIS/Assets/Art/Characters/animations/playerMovement_town.cs
@@ -25,6 +25,11 @@ public class PlayerMovement : MonoBehaviour
     Door currentDoor = null; //referece to the door that player is near
     public TextMeshProUGUI interactText; // Assign in Inspector
 
+    //NPC
+    bool nearNPC = false; //is player near NPC
+    NPC currentNPC = null; //reference to the NPC that player is near
+    public TextMeshProUGUI dialogueText; // Assign in Inspector - shows NPC dialogue
+
     public Sprite left, right, up, downleft, downright;
 
     private bool lookingLeft = false;
@@ -44,13 +49,23 @@ public class PlayerMovement : MonoBehaviour
 
         Move();
         Interact();
+
+        if (nearNPC && currentNPC != null && Input.GetKeyDown(KeyCode.T))
+{
+    ShowNPCPreviewText();
+}
     }
 
     void Interact()
     {
-        if (nearDoor && currentDoor != null && Input.GetKeyDown(KeyCode. E))
+        if (nearDoor && currentDoor != null && Input.GetKeyDown(KeyCode.E))
         {
             EnterDoor();
+        }
+
+        if (nearNPC && currentNPC != null && Input.GetKeyDown(KeyCode.E))
+        {
+            TalkToNPC();
         }
     }
 
@@ -59,32 +74,53 @@ public class PlayerMovement : MonoBehaviour
         SceneManager.LoadScene(currentDoor.sceneToLoad);
     }
 
+    void TalkToNPC()
+    {
+        if (dialogueText == null || currentNPC == null)
+            return;
+
+        // Open or close NPC dialogue WITHOUT touching interactText
+        if (!dialogueText.gameObject.activeSelf)
+        {
+            dialogueText.text = currentNPC.dialogueText;
+            dialogueText.gameObject.SetActive(true);
+        }
+        else
+        {
+            dialogueText.gameObject.SetActive(false);
+        }
+    }
+
+
+
     void Move()
     {
-        Vector2 move;
+        Vector2 move = Vector2.zero;
 
-        if (Input.GetKey(KeyCode. A)) move.x = -speed;
-        else if (Input.GetKey(KeyCode. D)) move.x = speed;
+        if (Input.GetKey(KeyCode.A)) move.x = -speed;
+        else if (Input.GetKey(KeyCode.D)) move.x = speed;
         else move.x = 0;
 
-        if (Input.GetKey(KeyCode. W)) move.y = speed;
-        else if (Input.GetKey(KeyCode. S)) move.y = -speed;
+        if (Input.GetKey(KeyCode.W)) move.y = speed;
+        else if (Input.GetKey(KeyCode.S)) move.y = -speed;
         else move.y = 0;
 
         if (move.x < 0)
         {
             spriteRenderer.sprite = left;
             lookingLeft = true;
-        } else if (move.x > 0)
-            {
-                spriteRenderer.sprite = right;
-                lookingLeft = false;
-            }
+        }
+        else if (move.x > 0)
+        {
+            spriteRenderer.sprite = right;
+            lookingLeft = false;
+        }
 
         if (move.y > 0) // moving UP
         {
             spriteRenderer.sprite = up;
-        }else if (move.y < 0)
+        }
+        else if (move.y < 0)
         {
             spriteRenderer.sprite = (lookingLeft) ? downleft : downright;
         }
@@ -106,20 +142,41 @@ public class PlayerMovement : MonoBehaviour
         //    }
         //}
 
+
         if (other.CompareTag("Door"))
         {
-
             currentDoor = other.GetComponent<Door>();
+
             if (currentDoor != null)
             {
                 nearDoor = true;
-                if (interactText != null)
-                {
-                    interactText.text = $"Press E to Enter";
-                    interactText.gameObject.SetActive(true);
-                }
+                nearNPC = false;
+                currentNPC = null;
+
+                if (dialogueText != null)
+                    dialogueText.gameObject.SetActive(false);
+
+                interactText.text = "Press E to Enter";
+                interactText.gameObject.SetActive(true);
             }
         }
+
+        if (other.CompareTag("NPC"))
+{
+    currentNPC = other.GetComponent<NPC>();
+
+    if (currentNPC != null)
+    {
+        nearNPC = true;
+        nearDoor = false;
+        currentDoor = null;
+
+        interactText.text = "Press T to Talk";
+        interactText.gameObject.SetActive(true);
+    }
+}
+
+
     }
     void OnTriggerExit2D(Collider2D other)
     {
@@ -127,8 +184,33 @@ public class PlayerMovement : MonoBehaviour
         {
             nearDoor = false;
             currentDoor = null;
-            if (interactText != null)
+
+            if (!nearNPC)
                 interactText.gameObject.SetActive(false);
         }
+
+        if (other.CompareTag("NPC"))
+{
+    nearNPC = false;
+    currentNPC = null;
+
+    if (dialogueText != null)
+        dialogueText.gameObject.SetActive(false);
+
+    interactText.text = "Press T to Talk"; // reset it
+    interactText.gameObject.SetActive(false); // hide it since we're leaving
+}
+
     }
+
+    void ShowNPCPreviewText()
+{
+    if (interactText == null || currentNPC == null)
+        return;
+
+    // Show the NPC's text as the prompt temporarily
+    interactText.text = currentNPC.dialogueText;
+    interactText.gameObject.SetActive(true);
+}
+
 }
